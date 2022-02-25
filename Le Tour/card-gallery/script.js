6d0c7fe90c0005910da02362adb8fc398e5388f0
Emre Keleş <emrekeless@gmail.com>
2022-02-25 23:21:34 +0100

    trivia-joke

--- Le Tour/card-gallery/script.js
+++ Le Tour/card-gallery/script.js
@@ -1,7 +1,6 @@
 const panels = document.querySelectorAll(".panel")
 
 panels.forEach((panel) => {
-  console.log(panel)
   panel.addEventListener("click", () => {
     removeActiveClasses()
     panel.classList.add("active")
